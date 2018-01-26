66537b44bc39e715b233da2271c4dbe373416448
Gabriel Dumitrescu <gdumitrescu@users.noreply.github.com>
2018-01-26 02:58:05 +0000

    Add format and lint tasks
    use:
    - prettier
    - eslint

--- src/template-strings/02-multiline.spec.js
+++ src/template-strings/02-multiline.spec.js
@@ -2,20 +2,19 @@
 // To do: make all tests pass, leave the asserts unchanged!
 var assert = require('assert');
 
-describe('template string, can contain multiline content', function () {
-  it('a normal string can`t span across multiple lines', function () {
-    var normalString = 'line1\n' +
-      'line2';
+describe('template string, can contain multiline content', function() {
+  it('a normal string can`t span across multiple lines', function() {
+    var normalString = 'line1\n' + 'line2';
     assert.equal(normalString, 'line1\nline2');
   });
 
-  it('wrapped in backticks it can span over multiple lines', function () {
+  it('wrapped in backticks it can span over multiple lines', function() {
     var templateString = `line1
 line2`;
     assert.equal(templateString, 'line1\nline2');
   });
 
-  it('even over more than two lines', function () {
+  it('even over more than two lines', function() {
     var multiline = `line 1
                      line 2
                      line 3
@@ -23,16 +22,16 @@ line2`;
     assert.equal(multiline.split('\n').length, 4);
   });
 
-  describe('and expressions inside work too', function () {
+  describe('and expressions inside work too', function() {
     var x = 42;
 
-    it('like simple variables', function () {
+    it('like simple variables', function() {
       var multiline = `line 1
           ${x}`;
       assert.equal(multiline, 'line 1\n          42');
     });
 
-    it('also here spaces matter', function () {
+    it('also here spaces matter', function() {
       var multiline = `
 ${x}`;
       assert.equal(multiline, '\n42');
